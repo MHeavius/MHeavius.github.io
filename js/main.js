41c92c0660293f92b193664387a4abd9ff679ea7
Mark <mark.khariton@gmail.com>
2018-12-21 12:43:48 +0500

    Stuff

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready( function () {
+    var $toggler = $('.nav-toggler');
+    $toggler.click(function () {
+        console.log('hi');
+        var $target = $($(this).attr('data-target'));
+        $target.toggleClass('open');
+    });
+
+    $(".carousel .owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        mouseDrag: false,
+        touchDrag: false,
+        // autoplay: true,
+        // autoplayTimeout: 5000
+
+    });
+});
