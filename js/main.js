a96b04e732dcc483e314a1dadb1dc188d69fd724
Mark <mark.khariton@gmail.com>
2018-12-23 12:37:56 +0500

    Finishing touches 1

--- js/main.js
+++ js/main.js
@@ -9,18 +9,36 @@ $(document).ready( function () {
     $(".carousel .owl-carousel").owlCarousel({
         items: 1,
         loop: true,
-        // mouseDrag: false,
-        // touchDrag: false,
-        // autoplay: true,
-        // autoplayTimeout: 5000
-        
+        mouseDrag: false
     });
     
     
     $(".testimonial-slider").owlCarousel({
         items: 1,
         loop: true,
-        nav: true,
+        mouseDrag: false,
+        responsive:{
+            0: {
+                nav: false,
+                dots: true
+            },
+            767: {
+                nav: true
+            }
+        }
+    });
+
+    $(".testimony-btn").on('click', function(){
+        var $self = $(this);
+        var $parent = $($self.parent());
+        var $hidden = ($parent.find('.testimony-hidden'));
+        if ($hidden.hasClass('open')){
+            $hidden.removeClass('open');
+            $self.text('Подробнее');
+        } else {
+            $hidden.addClass('open');
+            $self.text('Скрыть');
+        }
     });
 
     $("#lightgallery").lightGallery();
